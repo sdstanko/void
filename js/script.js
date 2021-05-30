3f25eb5a6753aa0ed283641a8f96a7e9012c1762
sdstanko <64140298+sdstanko@users.noreply.github.com>
2021-05-30 20:57:53 +0300

    layot for pc and mobile done, navigation done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__content').toggleClass('active');
-		$('body').toggleClass('lock');
+// $(document).ready(function() {
+// 	$('.header__burger').click(function(event) {
+// 		$('.header__burger,.header__content').toggleClass('active');
+// 		$('body').toggleClass('lock');
+// 	});
+// });
+
+const iconBurger = document.querySelector('.header__burger');
+const headerContent = document.querySelector('.header__content');
+if (iconBurger){
+	iconBurger.addEventListener("click", function(e) {
+		document.body.classList.toggle('lock');
+		iconBurger.classList.toggle('active');
+		headerContent.classList.toggle('active');
 	});
-});
+}
+
+const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			if(iconBurger.classList.contains('active')) {
+				document.body.classList.remove('lock');
+				iconBurger.classList.remove('active');
+				headerContent.classList.remove('active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
